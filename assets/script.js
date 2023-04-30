6e9d1b0cc196c583c1fb7891c477c73fe4d6169a
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-04-30 16:21:43 -0500

    Added time divs and attempt timecheck loop

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,19 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  function timeCheck() {
+    var hour = dayjs().hour();
+    var timeBlock = $('.time-block')
+
+    timeBlock.each(function() {
+      var timeblockHour = $("id").attr(this);
+
+    })
+
+  }
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
