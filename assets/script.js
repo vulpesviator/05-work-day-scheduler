05353393dd9e71cc1e938b69196c88845e2e3ac4
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-05-02 11:57:46 -0500

    Cleaned up comments. Closes #10

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@
 // in the html.
 $(function () {
 
+  // This for loop creates a div row for each time block in a normal workday and then populates the description area with any saved events in Local Storage  
   for (var i = 9; i <= 17; i++) {
     var hourId = "hour-" + i;
     var idVal = localStorage.getItem('hour-' + i);
@@ -19,16 +20,9 @@ $(function () {
     
     $(`#hour-${i} .description`).val(idVal);
   }
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   
   var saveBtn = $(".saveBtn")
-
+  // This function listens to when the save button on a time block is clicked and saved the input to local storage with the hour id
   saveBtn.on("click", function(event) {
     event.preventDefault();
     var id = $(this).parent().attr("id");
@@ -37,14 +31,7 @@ $(function () {
     localStorage.setItem(id, description);
   })
 
-  
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  // This function checks the current hour and adds the appropriate class to a timeblock for whether it is in past, the present hour, or in the future
   function timeCheck() {
     var hour = dayjs().hour();
     var timeBlock = $('.time-block')
@@ -65,16 +52,9 @@ $(function () {
   }
 
   timeCheck();
-
+  // Interval checks the time every 15 minutes
   setInterval(timeCheck, 900000);
 
-    //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  
-
-  // TODO: Add code to display the current date in the header of the page.
+  // Displays the current day and date in the header
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
